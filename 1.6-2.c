231c81a2c43a6de1f89e6bfb0040b1e4e2de6aa4
Libertas <horizonvei@gmail.com>
2014-08-19 21:25:42 +0000

    1.6-2

---
+++ 1.6-2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BITSPERWORD 32
+#define N 1000000
+
+int *string;
+
+int setbit(int i)
+{
+    int con, left;
+    int *p;
+    con = i / BITSPERWORD;
+    left = i % BITSPERWORD;
+    p = string + con;
+    *p = *p | ((int)1 << left);
+    return 0;
+}
+
+int printstring()
+{
+    for(int i = 0; i < N / BITSPERWORD; i++)
+    {
+        int *p;
+        p = string + i;
+        for(int j = 0; j < BITSPERWORD; j++)
+        {
+            int tmp, tmp1;
+            tmp = *p;
+            tmp1 = (tmp << ((BITSPERWORD - 1) - j));
+            if(tmp1 >> (BITSPERWORD - 1))
+            {
+                printf("%d ", i * BITSPERWORD + j);
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
+
+int main()
+{
+    int tmp;
+    string = malloc((N/BITSPERWORD + 1) * sizeof(int));
+    for(int i = 0; i < N/BITSPERWORD + 1; i++) string[i] = 0;
+    while(scanf("%d", &tmp) != EOF)
+        setbit(tmp);
+    printstring();
+    free(string);
+    return 0;
+}
+
+
+//Don't use pointers of char
+//Do use definition
+//Good job~
